901652680451efc911fcc889f9ab317947962b2b
David Hernández Martín <17512929+belcebus@users.noreply.github.com>
2023-04-29 19:30:08 +0200

    asd

--- main.ts
+++ main.ts
@@ -1,35 +1,37 @@
 
 module.exports = ({github, context}) => {
+    
+    console.log(context.payload.issue.body);
+    console.log("******************");
+    let noResponse = "_No response_";
+
+    let lineas = context.payload.issue.body.split("\n");
+    let repoName = lineas[2];
+    let repoDescription = lineas[6]
+    let adminTeam = lineas[10]
+
+    if (repoDescription == noResponse){
+    repoDescription = "";
+    }
+    if (adminTeam == noResponse){
+    core.setFailed("Admin team es mandatory")
+    return
+    }
+
+    console.log("Repository name: " + repoName)
+    core.debug("Repository description: " + repoDescription)
+    core.debug("Admin team: " + adminTeam)
+
+    const regex = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}/i;
+    if (regex.test(repoName) && repoName.startsWith("gln-")) {
+    core.info('El nombre del repositorio solicitado comienza con el prefijo y tiene un formato válido');
+
+    } else {
+    core.setFailed(`El nombre del repositorio ${repoName} no cumple con los requisitos`);
+    }
     return context.payload.issue
   }
 
 /*
-console.log(context.payload.issue.body);
-console.log("******************");
-let noResponse = "_No response_";
 
-let lineas = context.payload.issue.body.split("\n");
-let repoName = lineas[2];
-let repoDescription = lineas[6]
-let adminTeam = lineas[10]
-
-if (repoDescription == noResponse){
-  repoDescription = "";
-}
-if (adminTeam == noResponse){
-  core.setFailed("Admin team es mandatory")
-  return
-}
-
-console.log("Repository name: " + repoName)
-core.debug("Repository description: " + repoDescription)
-core.debug("Admin team: " + adminTeam)
-
-const regex = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}/i;
-if (regex.test(repoName) && repoName.startsWith("gln-")) {
-  core.info('El nombre del repositorio solicitado comienza con el prefijo y tiene un formato válido');
-
-} else {
-  core.setFailed(`El nombre del repositorio ${repoName} no cumple con los requisitos`);
-}
 */
