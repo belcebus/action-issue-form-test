4ba8ed77c8f8cd20ea49a65aa865bef7e4c787b8
David Hernández Martín <belcebus@gmail.com>
2023-04-30 16:37:12 +0200

    crear comentario en la issue

--- main.js
+++ main.js
@@ -1,27 +1,39 @@
+/**
+ * Script que se ejecuta cuando se crea una issue en el repositorio
+ * analiza el contenido de la issue y crea un repositorio en la organización
+ * con el nombre y descripción indicados en la issue.
+ * 
+ */
 
 module.exports = ({github, context, core}) => {
 
-    core.debug(context.payload.issue.body);
+    core.debug(context.payload.issue.body)
     
-    const noResponse = "_No response_";
-    const prefix = "gln-";
-    const repoNamePos = 2;
-    const repoDescriptionPos = 6;
+    const noResponse = "_No response_"
+    const prefix = "gln-"
+    const repoNamePos = 2
+    const repoDescriptionPos = 6
     const adminTemaPos = 10
    
-    let lineas = context.payload.issue.body.split("\n");
-    let repoName = lineas[repoNamePos];
+    let lineas = context.payload.issue.body.split("\n")
+    let repoName = lineas[repoNamePos]
     let repoDescription = lineas[repoDescriptionPos]
     let adminTeam = lineas[adminTemaPos]
 
     if (repoDescription == noResponse){
       //Establecemos el valor a vacío en lugar de _No repoonse_
-      repoDescription = "";
+      repoDescription = ""
     }
     if (adminTeam == noResponse){
       //El team de administradores del repositorio es obligatorio
       core.setFailed("Admin team es mandatory")
       //TODO: aprovechar y crear un comentario en la issue avisando del error
+      github.issue.createComment({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: context.payload.issue.number,
+        body: "Admin team es mandatory"
+      })
       return
     }
 
@@ -29,13 +41,14 @@ module.exports = ({github, context, core}) => {
     core.info("Repository description: " + repoDescription)
     core.info("Admin team: " + adminTeam)
 
-    const regex = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}/i;
+    const regex = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}/i
     if (regex.test(repoName) && repoName.startsWith(prefix)) {
-      core.info('El nombre del repositorio solicitado comienza con el prefijo y tiene un formato válido');
+      core.info('El nombre del repositorio solicitado comienza con el prefijo y tiene un formato válido')
       //TODO: crear el repositorio para retornar la url
     } else {
-      core.setFailed(`El nombre del repositorio ${repoName} no cumple con los requisitos`);
+      core.setFailed(`El nombre del repositorio ${repoName} no cumple con los requisitos`)
     }
     //TODO: retornar la url del repositorio creado y cerrar la issue
     return context.payload.issue 
   }
+
