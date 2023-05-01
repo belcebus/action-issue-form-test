c6f03c9b37e4aeb7eb5a20bd457b01970aaf7426
David Hernández Martín <belcebus@gmail.com>
2023-05-01 21:53:17 +0200

    refactor

--- main.js
+++ main.js
@@ -11,9 +11,11 @@
  * 
  */
 
-module.exports = async ({ github, context, core }) => {
+module.exports = async ({ github, context, core, token }) => {
 
   core.debug(context.payload.issue.body)
+
+  console.log("-> " + token)
 
   const noResponse = "_No response_"
   const prefix = "gln-"
