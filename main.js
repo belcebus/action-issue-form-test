53cf059defa3eedbd3b496d13e16d679807c666c
David Hernández Martín <belcebus@gmail.com>
2023-05-01 20:42:07 +0200

    refactor

--- main.js
+++ main.js
@@ -11,16 +11,16 @@
  * 
  */
 
-module.exports = async ({github, context, core}) => {
+module.exports = async ({ github, context, core }) => {
 
   core.debug(context.payload.issue.body)
-  
+
   const noResponse = "_No response_"
   const prefix = "gln-"
   const repoNamePos = 2
   const repoDescriptionPos = 6
   const adminTemaPos = 10
-  
+
   let lineas = context.payload.issue.body.split("\n")
   let repoName = lineas[repoNamePos].trim()
   let repoDescription = lineas[repoDescriptionPos].trim()
@@ -28,30 +28,29 @@ module.exports = async ({github, context, core}) => {
   let adminTeamId = 0
   let newRepoUrl = ""
 
-
   // inicializamos una lista con los errors encontrados
   let errors = []
 
   //Comprobamos que los campos obligatorios están informados
-  if (adminTeam == noResponse || adminTeam == ""){
+  if (adminTeam == noResponse || adminTeam == "") {
     errors.push("Admin team is mandatory, update the issue")
-  }else{
+  } else {
     //Comprobamos que el team de administradores existe en la organización
     // y recuperamos su id
     console.log("Admin team: " + adminTeam)
     try {
-      const { data: team } =  await github.rest.teams.getByName({
+      const { data: team } = await github.rest.teams.getByName({
         org: context.repo.owner,
         team_slug: adminTeam
       })
-      adminTeamId=team.id
+      adminTeamId = team.id
       core.info("Admin team " + adminTeam + " exists in the organization, id: " + adminTeamId)
-    }catch (error){
+    } catch (error) {
       errors.push("Admin team " + adminTeam + " does not exist in the organization, update the issue. Error: " + error)
       console.log(error)
     }
   }
-  
+
   //Comprobamos que el nombre del repositorio cumple con los requisitos
   const regex = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}/i
   if (!regex.test(repoName) || !repoName.startsWith(prefix)) {
@@ -65,14 +64,14 @@ module.exports = async ({github, context, core}) => {
       repo: repoName
     })
     errors.push("Repository " + repoName + " already exists in the organization, update the issue")
-  }catch (error){
+  } catch (error) {
     core.info("Repository " + repoName + " does not exist in the organization")
   }
 
   //Procesamos la lista de errors de validación previa
-  if (errors.length > 0){
+  if (errors.length > 0) {
     let body = ""
-    for(error of errors){
+    for (error of errors) {
       body += ":x: " + error + "\n"
     }
     //Crear un comentario en la issue avisando del error
@@ -87,22 +86,22 @@ module.exports = async ({github, context, core}) => {
   }
 
   //Establecemos el valor a vacío en lugar de _No repoonse_ en los campos opcionales
-  if (repoDescription == noResponse){
+  if (repoDescription == noResponse) {
     repoDescription = ""
   }
-    
+
   //Validaciones previas correctas, se puede crear el repositorio
   core.info("Issue number: " + context.payload.issue.number)
   core.info("Repository name: " + repoName)
   core.info("Repository description: " + repoDescription)
   core.info("Admin team: " + adminTeam)
   core.info("Admin team id: " + adminTeamId)
-  
+
   core.info("Creating repository " + repoName + " in organization " + context.repo.owner)
-  
+
   try {
     //crear el repositorio en la organización
-    const {data: repo} = await github.rest.repos.createInOrg({
+    const { data: repo } = await github.rest.repos.createInOrg({
       org: context.repo.owner,
       name: repoName,
       description: repoDescription,
@@ -142,7 +141,7 @@ module.exports = async ({github, context, core}) => {
       comment: "Repository " + repoName + " created in organization " + context.repo.owner
     })
   }
-  catch (error){
+  catch (error) {
     core.setFailed("Error creating repository " + repoName + " in organization " + context.repo.owner + ". Error: " + error)
     github.rest.issues.createComment({
       owner: context.repo.owner,
@@ -156,7 +155,7 @@ module.exports = async ({github, context, core}) => {
   }
 
   core.info("Repository " + repoName + " created in organization " + context.repo.owner)
-  core.info("retornando la url del repositorio creado: "+ newRepoUrl)
+  core.info("retornando la url del repositorio creado: " + newRepoUrl)
 
   return repoName
 }
