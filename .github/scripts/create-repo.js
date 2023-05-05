b05d5bbd006632778ea5ea6ca9a680e2db13eafa
David Hernández Martín <belcebus@gmail.com>
2023-05-05 22:11:48 +0200

    nuevas opciones de repositorio

--- .github/scripts/create-repo.js
+++ .github/scripts/create-repo.js
@@ -164,6 +164,7 @@ module.exports = async ({ github, context, core }) => {
         description: repoDescription,
         private: true
       })
+      core.debug("createInOrg response: " + JSON.stringify(repo))
       newRepoUrl = repo.html_url
     }else if(sourceType == sourceTypeFork){
       const { data: repo } = await github.rest.repos.createFork({
@@ -172,6 +173,7 @@ module.exports = async ({ github, context, core }) => {
         organization: context.repo.owner,
         name: repoName
       })
+      core.debug("createFork response: " + JSON.stringify(repo))
       newRepoUrl = repo.html_url
     }else if(sourceType == sourceTypeTemplate){
       const { data: repo } = await github.rest.repos.createUsingTemplate({
@@ -182,6 +184,7 @@ module.exports = async ({ github, context, core }) => {
         description: repoDescription,
         private: true
       })
+      core.debug("createUsingTemplate response: " + JSON.stringify(repo))
       newRepoUrl = repo.html_urls
     }else{
       //la opción de tipo de fuente no es ninguna de las anteriores, no debería llegar aquí
